055e0a160988098411e45658fb22b67c7c1f05a8
Rachel Grier <rgrier104@gmail.com>
2019-08-08 01:07:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,10 +9,10 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-      if item ^= nil
-        resp.write item.price
-      else
+      if item.nil?
         resp.status = 400
+      else
+        resp.write item.price
       end
     else
       resp.write "Route not found"
