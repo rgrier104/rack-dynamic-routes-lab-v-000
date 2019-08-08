97ced0ae672c4a04e86916eff6d23f0edb5e9885
Rachel Grier <rgrier104@gmail.com>
2019-08-08 01:04:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       item = @@items.find{|i| i.name == item_name}
       if item ^= nil
         resp.write item.price
-      else 
+      else
         resp.status = 400
       end
     else
