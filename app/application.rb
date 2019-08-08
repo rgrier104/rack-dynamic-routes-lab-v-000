60c6e1c49819c7e23ba3ed74f91445ebabe2c138
Rachel Grier <rgrier104@gmail.com>
2019-08-08 00:58:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,14 @@
 class Application
 
+@@items = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
+      item_price = req.path.split("/items/").last
+      
       resp.write "You requested the songs"
     else
       resp.write "Route not found"
